1c3465a48712ebf96a242704c92ac52d5d05bcc8
Przemysław Czerkas <pczerkas@gmail.com>
2022-07-13 22:08:21 +0200

    MAX_MINUTES

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # shellcheck disable=SC2009
-ps x | grep -E "$MAX_CPU_MINUTES:.. ffmpe[g]" | awk '{print $1}' | xargs -I {} -r kill "{}" || true
+ps -eo pid,etime,comm | grep -E "$MAX_MINUTES:.. ffmpe[g]" | awk '{print $1}' | xargs -I {} -r kill "{}" || true
 
 if ! (netstat -an | grep ":$CONTROLLER_PORT" >/dev/null);
 then
